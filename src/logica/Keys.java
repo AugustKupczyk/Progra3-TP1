7de44e872316cb5421f9325d80309c688b0b4a38
FranciscoJulian32 <franciscojulian32@outlook.com>
2024-04-05 00:43:25 -0300

    Se realizaron cambios en el paquete Logica

---
+++ src/logica/Keys.java
@@ -0,0 +1,74 @@
+package logica;
+
+public class Keys extends LogicaMatriz {
+	public Keys(int dimension) {
+		super(dimension);
+		// TODO Auto-generated constructor stub
+	}
+
+	public boolean moveRight(Integer [][] mat , boolean moved) {
+		for(int row=0;row < mat.length;row++) {
+			for (int col = mat[0].length-2; col >= 0;col--) {
+				if (mat [row][col]!=0) {
+					int k = col+1;
+					while (k <mat.length && mat [row][k]==0) {
+						k++;
+					}
+					if (k < mat.length && mat [row][k]==mat [row][col]) {
+						mat [row][k]*=2 ; 
+						mat [row][col]= 0 ; 
+						moved = true;
+					}else if (k-1 != col) {
+						mat [row][k-1]=mat [row][col]; 
+						mat [row][col]= 0 ; 
+						moved = true;
+					}
+				}
+			}
+		}
+		return moved; 
+	}
+	public boolean moveDown(Integer [][] mat,boolean moved) {
+		for (int col = 0; col < mat.length ; col++) {
+			for (int row = mat[0].length-2 ; row >= 0 ; row--){
+				if (mat[row][col] != 0) {
+					int k = row + 1;
+					while (k < mat.length && mat [k][col]==0) {
+						if (k < mat.length && mat[k][col] == mat[row][col]) {
+		                    mat[k][col] *= 2;
+		                    mat[row][col] = 0;
+		                    moved = true;
+		                } else if (k - 1 != row) {
+		                    mat[k - 1][col] = mat[row][col];
+		                    mat[row][col] = 0;
+		                    moved = true;
+					}
+				}
+			}
+		}
+	}
+		return moved;
+}
+	public boolean moveUp(Integer [][] mat,boolean moved) {
+	    for (int col = 0; col < mat.length; col++) {
+	        for (int row = 1; row < mat[0].length; row++) {
+	            if (mat[row][col] != 0) {
+	                int k = row - 1;
+	                while (k >= 0 && mat[k][col] == 0) {
+	                    k--;
+	                }
+	                if (k >= 0 && mat[k][col] == mat[row][col]) {
+	                    mat[k][col] *= 2;
+	                    mat[row][col] = 0;
+	                    moved = true;
+	                } else if (k + 1 != row) {
+	                    mat[k + 1][col] = mat[row][col];
+	                    mat[row][col] = 0;
+	                    moved = true;
+	                }
+	            }
+	        }
+	    }
+	    return moved;
+	}
+}
--- src/logica/LogicaMatriz.java
+++ src/logica/LogicaMatriz.java
@@ -1,5 +1,102 @@
 package logica;
 
-public class LogicaMatriz {
+import java.awt.event.KeyEvent;
+import java.util.Random;
+import javax.swing.JOptionPane;
+import interfaz.VentanaJuego;
 
+public class LogicaMatriz {
+	private Integer [][] matTablero; //TENDRIA QUE CONTENER LOS LABELS
+	private Random random;
+	private VentanaJuego tablero;
+	private Keys keyInstance;
+	private Integer dimension;
+	
+	public LogicaMatriz (int dimension) {
+		this.dimension=4;
+		tablero = new VentanaJuego();
+		keyInstance = new Keys(dimension);
+		matTablero = new Integer [dimension][dimension]; 
+	}
+	
+	private void generarFichas () {
+		int celdasVacias= cantCeldasVacias();
+		if (celdasVacias == 0) {
+			return;
+		}else {
+		int indice = random.nextInt(celdasVacias)+1;
+		celdasVacias = 0;
+		
+		for (int row = 0; row < matTablero.length; row++ ) {
+			for (int col = 0 ; col <matTablero[0].length;col++) {
+				celdasVacias++;
+				if (celdasVacias == indice) {
+					int valor = (random.nextDouble() < 0.9) ? 2 : 4;
+					matTablero[row][col]=valor;
+					return;
+				}
+			}
+		}
+		
+		
+		}
+	}
+	private boolean moveTiles (int direction) { //Mover Fichas 
+		boolean moved = false;
+		switch(direction) {
+		case KeyEvent.VK_UP:
+			moved = keyInstance.moveUp(matTablero,moved);
+			break;
+		case KeyEvent.VK_DOWN:
+			moved = keyInstance.moveDown(matTablero,moved);
+			break;
+		case KeyEvent.VK_LEFT:
+			moved = keyInstance.moveLeft(matTablero,moved);
+			break;
+		case KeyEvent.VK_RIGHT:
+			moved = keyInstance.moveRight(matTablero,moved);
+			break;
+		}
+		if (moved) {
+			generarFichas();
+			if (isGameOver()) {
+				JOptionPane.showMessageDialog(this, "GameOver","2048");
+			}
+		}
+	}
+	private void combinarFichas(int fila, int col, int newFila, int newCol) {
+		int actualValor = matTablero [fila][col];
+		matTablero [fila][col]= 0;
+		matTablero [newFila][newCol]*=2; 
+		
+		if (matTablero[newFila][newCol]==2048) {
+			isGameOver();
+		}
+		generarFichas();
+		
+	}
+//	private boolean isGameOver() {
+//		return matTablero.gano()||matTablero.perdio();
+//	}
+	private int cantCeldasVacias () {
+		int cantVacias = 0;
+		for (int row=0; row < matTablero.length; row++) {
+			for (int col = 0; col < matTablero.length; col++ ) {
+				if (matTablero[row][col]==0) {
+					cantVacias++;
+				}
+			}
+		}
+		return cantVacias;
+	}
+//OTRA FORMA 	
+//	int valor = (random.nextDouble() < 0.9) ? 2 : 4;
+//	do {
+//		fila = random.nextInt(4);
+//		col = random.nextInt(4);
+//	}while (matTablero[fila][col]!= 0 ); //tablero.Lugar == null
+//	matTablero[fila][col]= valor;
 }
+
+
+
